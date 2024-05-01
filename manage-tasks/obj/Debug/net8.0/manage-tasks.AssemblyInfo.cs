dcb06f33040a0c102978b8c29a6bc8dbf18c49f3
Tyler Simeone <tylersimeone@Tylers-MacBook-Pro.local>
2024-04-30 22:56:09 -0500

    Merge latest from master

--- manage-tasks/obj/Debug/net8.0/manage-tasks.AssemblyInfo.cs
+++ manage-tasks/obj/Debug/net8.0/manage-tasks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("manage-tasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783028ae4a77378be0f220b62afde6605b9458d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f7bb35a572129233f18dec64f66f90a5a4ef1c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("manage-tasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("manage-tasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
